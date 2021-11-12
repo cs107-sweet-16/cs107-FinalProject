907e932fbdd378604be9c1ea76085d42663cf5bd
annaw <anna_wuest@dfci.harvard.edu>
2021-11-12 11:10:22 -0500

    added some functions to the dual number class with Bhawesh

--- src/dualnumber.py
+++ src/dualnumber.py
@@ -4,6 +4,8 @@ class Dualnumber:
     '''
 
     def __init__(self, a):
+        import numpy as np
+        from dualnumber import Dualnumber
         self.val = a
         self.der = 1
 
@@ -38,7 +40,60 @@ class Dualnumber:
         return self * other
 
     def __sub__(self, other):
-        pass
+        try:
+            subtraction = Dualnumber(self.val - other.val)
+            subtraction.der = self.der - other.der
+            return subtraction
+        except AttributeError as e:
+            other = Dualnumber(other)
+            subtraction = Dualnumber(self.val - other.val)
+            subtraction.set_dual(self.der)
+            return subtraction
+
+    def __rsub__(self, other):
+        return self - other
 
     def __div__(self, other):
+        try:
+            div = Dualnumber(self.val/other.val)
+            div.der = {self.der*other.val - self.val*other.der}/(other.val**2)
+        except AttributeError as e:
+            div = Dualnumber(self.val / other)
+            div.set_dual(self.der / other)
+            return div
+
+    def __rdiv__(self, other):
+        return self/other
+
+    def __pow__(self, other):
+        try:
+            # dual nuumber raised to dual number
+            pow = Dualnumber(self.val**other.val)
+            pow.der = {other.val*{self.val**(other.val-1)}*self.der} + {np.log(self.val)}*{self.val**(other.val)}*other.der
+            return pow
+        except AttributeError as e:
+            # dual number raised to real number d^r
+            pow = Dualnumber(self.val**other)
+            pow.der = {other*self.val**(other-1)}*self.der
+            return pow
+
+    ## check reverse power
+    def __rpow__(self, other):
         pass
+
+
+
+if __name__ == '__main__':
+    # test passing 2 dual numbers to add
+    x = Dualnumber(1)
+    y = Dualnumber(2)
+    z = x + y
+    assert z.val == 3
+    assert z.der == 2
+
+    # test passing 1 dual and 1 non dual number to add
+    x = Dualnumber(4)
+    y = 5
+    z = x + y
+    assert z.val == 9
+    assert z.der == 1
