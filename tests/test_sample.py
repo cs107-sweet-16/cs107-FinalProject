6161d4c2e34e70dbb2d1fbba4ce813697e96cdae
Albert Ge <lbertge@gmail.com>
2021-11-18 00:00:30 -0500

    Add initial test on dual numbers

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,18 +2,18 @@ import pytest
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-# from dualnumber import Dualnumber
+from dualnumber import Dualnumber
 
 def test_sample():
     assert True == True
 
-# def add_dualnumbers():
-    # # test passing 2 dual numbers to add
-    # x = Dualnumber(1)
-    # y = Dualnumber(2)
-    # z = x + y
-    # assert z.val == 3
-    # assert z.der == 2
+def add_dualnumbers():
+    # test passing 2 dual numbers to add
+    x = Dualnumber(1)
+    y = Dualnumber(2)
+    z = x + y
+    assert z.val == 3
+    assert z.der == 2
 
 if __name__ == '__main__':
     test_sample()
