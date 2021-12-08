5194e35b9a44b3628ab1686de9cfce2702e7e187
jfanghrv_local <jingxingfang99@gmail.com>
2021-12-08 16:37:29 -0500

    wrapped user interface for vectorization

--- src/node.py
+++ src/node.py
@@ -8,7 +8,7 @@ def sin(a):
         return funcNode(np.sin, np.cos, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.sin(a))
+        n.set_val(np.sin(a))
         return n
     else:
         raise TypeError
@@ -18,7 +18,7 @@ def cos(a):
         return funcNode(np.cos, lambda x: -np.sin(x), None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.cos(a))
+        n.set_val(np.cos(a))
         return n
     else:
         raise TypeError
@@ -28,7 +28,7 @@ def tan(a):
         return funcNode(np.tan, lambda x: 1/(np.cos(x))**2, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.tan(a))
+        n.set_val(np.tan(a))
         return n
     else:
         raise TypeError
@@ -38,7 +38,7 @@ def sinh(a):
         return funcNode(np.sinh, np.cosh, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.sinh(a))
+        n.set_val(np.sinh(a))
         return n
     else:
         raise TypeError
@@ -48,7 +48,7 @@ def cosh(a):
         return funcNode(np.cosh, np.sinh, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.cosh(a))
+        n.set_val(np.cosh(a))
         return n
     else:
         raise TypeError
@@ -58,7 +58,7 @@ def tanh(a):
         return funcNode(np.tanh, lambda x: 1-np.tanh(x)**2, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.tanh(a))
+        n.set_val(np.tanh(a))
         return n
     else:
         raise TypeError
@@ -71,7 +71,7 @@ def sqrt(a):
         return funcNode(np.sqrt, lambda x: 0.5/np.sqrt(x), None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.sqrt(a))
+        n.set_val(np.sqrt(a))
         return n
     else:
         raise TypeError
@@ -82,7 +82,7 @@ def exp(a):
         return funcNode(np.exp, np.exp, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.exp(a))
+        n.set_val(np.exp(a))
         return n
     else:
         raise TypeError
@@ -92,7 +92,7 @@ def ln(a):
         return funcNode(np.log, lambda x: 1/x, None, a, None)
     elif isinstance(a, int) or isinstance(a, float):
         n = valNode()
-        n._set_val(np.log(a))
+        n.set_val(np.log(a))
         return n
     else:
         raise TypeError
@@ -111,7 +111,7 @@ class Node:
             r = other
         elif isinstance(other, int) or isinstance(other, float):
             r = valNode()
-            r._set_val(other)
+            r.set_val(other)
         else:
             raise TypeError
         return funcNode(np.add,lambda x,y: 1, lambda x,y: 1, self, r)
@@ -125,7 +125,7 @@ class Node:
             r = other
         elif isinstance(other, int) or isinstance(other, float):
             r = valNode()
-            r._set_val(other)
+            r.set_val(other)
         else:
             raise TypeError
         return funcNode(np.multiply,lambda x,y: y, lambda x,y: x, self, r)
@@ -143,7 +143,7 @@ class Node:
             r = other
         elif isinstance(other, int) or isinstance(other, float):
             r = valNode()
-            r._set_val(other)
+            r.set_val(other)
         else:
             raise TypeError
         return funcNode(np.subtract,lambda x,y: 1, lambda x,y: -1, self, r)
@@ -156,7 +156,7 @@ class Node:
             r = other
         elif isinstance(other, int) or isinstance(other, float):
             r = valNode()
-            r._set_val(other)
+            r.set_val(other)
         else:
             raise TypeError
         return funcNode(np.divide, lambda x,y: 1/y, lambda x,y: -x/y/y, self, r)
@@ -164,7 +164,7 @@ class Node:
     def __rtruediv__(self, other):
         if isinstance(other, int) or isinstance(other, float):
             l = valNode()
-            l._set_val(other)
+            l.set_val(other)
         else:
             raise TypeError
         return funcNode(np.divide, lambda x,y: 1/y, lambda x,y: -x/y/y, l, self)               
@@ -174,7 +174,7 @@ class Node:
             r = other
         elif isinstance(other, int) or isinstance(other, float):
             r = valNode()
-            r._set_val(other)
+            r.set_val(other)
         else:
             raise TypeError("unsupported input type(s) for the operand")
         return funcNode(np.power, lambda x,y: y*np.power(x,y-1), lambda x,y: np.power(x,y)*np.log(x), self, r)
@@ -182,7 +182,7 @@ class Node:
     def __rpow__(self, other):
         if isinstance(other, int) or isinstance(other, float):
             l = valNode()
-            l._set_val(other)
+            l.set_val(other)
         else:
             raise TypeError
         return funcNode(np.power, lambda x,y: y*np.power(x,y-1), lambda x,y: np.power(x,y)*np.log(x), l, self)
@@ -200,7 +200,7 @@ class valNode(Node):
         # if name != None:
         #     self.der[name]=1
     
-    def _set_val(self, val):
+    def set_val(self, val):
         self.val = val
     def __str__(self):
         return str(self.name) if self.name != None else str(self.val)
@@ -221,6 +221,8 @@ class valNode(Node):
         self.der += partial*adjoint
         return {self.name: self}
 
+    def _reset_der(self):
+        self.der = 0
 
 class funcNode(Node):
     def __init__(self, func, leftdf, rightdf, left, right):
@@ -291,15 +293,15 @@ class funcNode(Node):
     
     
 class vector:
-    def __init__(self, name, size):
-        self.name = name
-        self.size = size
-        self.elements = [ valNode(self.name+str(i)) for i in range(self.size) ]
+    def __init__(self, *args):
+        self.size = len(args)
+        self.elements = args
     
-    def set_val(array):
+    def set_val(self, array):
         if len(array) != self.size:
             raise ValueError(f"Input size has a mismatch with the vector size ({self.size})")
         for node, val in zip(self.elements, array):
+            # print(val)
             node.set_val(val)
 
     def __getitem__(self, key):
@@ -308,6 +310,46 @@ class vector:
     def __iter__(self, key):
         return iter(self.elements)
     
+    def grad(self, var):
+        '''
+            auto-diff for vector functions
+            currently we only use reverse mode to calculate gradient for vector functions
+            input: the variable or vector variables var, which is used to
+             define the function and for which we need to calculate the derivatives
+            return: Jacobian matrix
+        '''
+        der_array = []
+        var._reset_der()
+        for f in self.elements:
+            f.forward_pass()
+            f.reverse_pass(1,1)
+            der_array.append(var.der)
+            var._reset_der()
+        return der_array
+    
+    def evaluate(self):
+        '''
+            evaluation for vector functions
+        '''
+        return [ f.forward_pass() for f in self.elements ]        
+    
+    @property
+    def der(self):
+        return [ node.der for node in self.elements ]
+    
+    def _reset_der(self):
+        '''
+           this function only works if it is a variable vector
+        '''
+        for node in self.elements:
+            node.der = 0
+    
+def variables(name, size = None):
+    if size == None or size == 1:
+        return valNode(name)
+    elems = [ valNode(name+'___'+str(i)) for i in range(size) ]
+    return vector(*elems)
+
     
 
             
@@ -315,25 +357,46 @@ if __name__=='__main__':
 
     
     # v = vector([1,2,3])
-    
-    def f(v):
-        '''
-            f takes a size=3 vector and output a size=2 vector
-        '''
-        f1 = sin(ln(v[0]))+tan(v[0]**2+v[0]*v[1]+v[2])
-        f2 = ln(sin(exp(v[0])+v[1])) + exp(v[1]) + v[2]
-        return f1,f2
 
-    x = valNode('x')
-    y = valNode('y')
-    c = valNode('c')
+    # example to define scalar function with multiple input variables
+
+    x = variables('x')
+    y = variables('y')
     f = sin(ln(x))+tan(x*x+y*x+x*x*x*y)
-    x._set_val(2)
-    y._set_val(3)
-    c._set_val(np.pi)
+    x.set_val(2)
+    y.set_val(3)
     print(f.forward())
     print(f.reverse())
     
+    # example to define vector function with vectorized input variables
+    
+    v = variables('v', 2)
+    v.set_val([2,3])    
+    print(v.evaluate())
+    print(v.grad(v))
+    def func(v):
+        '''
+            f takes a size=3 vector and output a size=2 vector
+        '''
+        f1 = sin(ln(v[0]))+tan(v[0]**2+v[0]*v[1]+v[0]**3*v[1])
+        f2 = v[0]*v[1]+v[1]**2+sqrt(v[0])
+        return vector(f1,f2)
+    f = func(v)
+    print(f.evaluate())
+    print(f.grad(v))
+        
+        
+    
+    # vector function can also take a scalar input variable
+    def func1(x):
+        f1 = x**2
+        f2 = x**3
+        return vector(f1, f2)
+    
+    f = func1(x)
+    print(f.evaluate())
+    print(f.grad(x))
+        
     '''x.der = 0
     y.der = 0
     f = log(sin(exp(x)+y)) + exp(y) + x
